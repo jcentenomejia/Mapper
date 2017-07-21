895ef1730be3d242e7fd221bb41711c7d724f4eb
jorge <jorgea_centeno@hotmail.com>
2017-07-21 20:39:20 +0200

    first test good

--- console_mapper/src/test/java/fr/epita/mapper/tests/GetFile.java
+++ console_mapper/src/test/java/fr/epita/mapper/tests/GetFile.java
@@ -2,11 +2,13 @@ package fr.epita.mapper.tests;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
 
 import fr.epita.mapper.models.Rule;
+import fr.epita.mapper.models.DrawingString;
 import fr.epita.mapper.services.RuleDao;
 import fr.epita.mapper.services.RulesFile;
 
@@ -18,11 +20,16 @@ public class GetFile {
 		//file.getFile();
 		List<Rule> rules = file.getRules();
 
+		List<DrawingString> drawing = new ArrayList<DrawingString>();
 		RuleDao dao = new RuleDao();
 		for(Rule rule:rules){
 			System.out.println(rule);
+			
+			drawing.addAll(dao.getDrawingStrings(rule));
 			dao.insertRule(rule);
 			
 		}
+		
+		System.out.println(drawing);
 	}
 }
